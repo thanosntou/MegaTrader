18e73174a33c7d565e58c78841540043bb5f2536
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2018-10-09 21:45:11 +0300

    fixed

--- src/main/java/com/ntouzidis/cooperative/module/user/entity/CustomerToTraderLink.java
+++ src/main/java/com/ntouzidis/cooperative/module/user/entity/CustomerToTraderLink.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class CustomerToTraderLink {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
