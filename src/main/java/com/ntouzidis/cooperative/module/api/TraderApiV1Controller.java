8886c4fa29c1bce43bc25ab30ce852532a8bcd87
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2018-12-26 19:00:54 +0200

    asdfasf

---
+++ src/main/java/com/ntouzidis/cooperative/module/api/TraderApiV1Controller.java
@@ -0,0 +1,39 @@
+package com.ntouzidis.cooperative.module.api;
+
+import com.ntouzidis.cooperative.module.user.entity.User;
+import com.ntouzidis.cooperative.module.user.service.UserService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/trader")
+public class TraderApiV1Controller {
+
+  @Value("${trader}")
+  private String traderName;
+
+  private final UserService userService;
+
+  @Autowired
+  public TraderApiV1Controller(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/followers")
+  public ResponseEntity<?> getFollowers() throws NotFoundException {
+
+    User trader = userService.findByUsername(traderName)
+            .orElseThrow(() -> new NotFoundException("Trader not found"));
+
+    List<User> followers = userService.getFollowers(trader);
+
+    return ResponseEntity.ok(followers);
+  }
+}
