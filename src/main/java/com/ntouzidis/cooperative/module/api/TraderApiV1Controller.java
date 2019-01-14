2239ec64173bdb23fcc4fb7baa59d8714456bb57
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-01-14 21:18:13 +0200

    activeOrders api

--- src/main/java/com/ntouzidis/cooperative/module/api/TraderApiV1Controller.java
+++ src/main/java/com/ntouzidis/cooperative/module/api/TraderApiV1Controller.java
@@ -1,18 +1,18 @@
 package com.ntouzidis.cooperative.module.api;
 
+import com.ntouzidis.cooperative.module.trade.TradeService;
 import com.ntouzidis.cooperative.module.user.entity.User;
 import com.ntouzidis.cooperative.module.user.service.UserService;
 import javassist.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/trader")
@@ -21,20 +21,37 @@ public class TraderApiV1Controller {
   @Value("${trader}")
   private String traderName;
 
-  private final UserService userService;
+  @Value("${superAdmin}")
+  private String superAdmin;
 
-  public TraderApiV1Controller(UserService userService) {
+  private final UserService userService;
+  private final TradeService tradeService;
+
+  public TraderApiV1Controller(UserService userService, TradeService tradeService) {
     this.userService = userService;
+    this.tradeService = tradeService;
   }
 
   @GetMapping("/followers")
-  public ResponseEntity<?> getFollowers() throws NotFoundException {
+  public ResponseEntity<?> getFollowers() {
 
     User trader = userService.findByUsername(traderName)
-            .orElseThrow(() -> new NotFoundException("Trader not found"));
+            .orElseThrow(() -> new RuntimeException("Trader not found"));
 
     List<User> followers = userService.getFollowers(trader);
 
     return ResponseEntity.ok(followers);
+  }
+
+  @GetMapping("/active_orders")
+  public ResponseEntity<List<Map<String, Object>>> getActiveOrders() {
+
+    User trader = userService.findByUsername(traderName)
+            .orElseGet(() -> userService.findByUsername(superAdmin)
+                    .orElseThrow(() -> new RuntimeException("Trader not found")));
+
+    List<Map<String, Object>> randomActiveOrders = tradeService.getRandomActiveOrders(trader);
+
+    return new ResponseEntity<>(randomActiveOrders, HttpStatus.OK);
   }
 }
