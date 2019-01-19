ae3da741421f0bd644a305fb6ff5774c853f72e5
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-01-19 20:16:11 +0200

    ref

--- src/main/java/com/ntouzidis/cooperative/module/api/TraderApiV1Controller.java
+++ src/main/java/com/ntouzidis/cooperative/module/api/TraderApiV1Controller.java
@@ -1,23 +1,15 @@
 package com.ntouzidis.cooperative.module.api;
 
-import com.ntouzidis.cooperative.module.common.builder.DataDeleteOrderBuilder;
-import com.ntouzidis.cooperative.module.common.builder.DataPostLeverage;
-import com.ntouzidis.cooperative.module.common.builder.DataPostOrderBuilder;
-import com.ntouzidis.cooperative.module.common.builder.SignalBuilder;
-import com.ntouzidis.cooperative.module.common.enumeration.Symbol;
 import com.ntouzidis.cooperative.module.trade.TradeService;
 import com.ntouzidis.cooperative.module.user.entity.CustomUserDetails;
 import com.ntouzidis.cooperative.module.user.entity.User;
 import com.ntouzidis.cooperative.module.user.service.UserService;
-import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 
@@ -67,72 +59,6 @@ public class TraderApiV1Controller {
     List<Map<String, Object>> randomActiveOrders = tradeService.getRandomPositions(trader);
 
     return new ResponseEntity<>(randomActiveOrders, HttpStatus.OK);
-  }
-
-  @PostMapping(value = "/signal")
-  public ResponseEntity<?> createSignal(@RequestParam(name="symbol", required = false) String symbol,
-                                        @RequestParam(name="side") String side,
-                                        @RequestParam(name="leverage", required = false) String leverage,
-                                        @RequestParam(name="stopLoss", required = false) String stopLoss,
-                                        @RequestParam(name="profitTrigger", required = false) String profitTrigger,
-                                        Authentication authentication)
-  {
-    User trader = ((CustomUserDetails) authentication.getPrincipal()).getUser();
-
-    if (symbol == null) symbol = "XBTUSD";
-
-    SignalBuilder signalBuilder = new SignalBuilder()
-            .withSymbol(Symbol.valueOf(symbol).getValue())
-            .withSide(side)
-            .withleverage(leverage)
-            .withStopLoss(stopLoss)
-            .withProfitTrigger(profitTrigger);
-
-    tradeService.createSignal(trader, signalBuilder);
-
-    return new ResponseEntity<>("okk", HttpStatus.OK);
-  }
-
-    @PostMapping(value = "/orderAll")
-    public ResponseEntity<?> postOrderAll(@RequestParam(name="symbol") String symbol,
-                                          @RequestParam(name="side") String side,
-                                          @RequestParam(name="ordType") String ordType,
-                                          @RequestParam(name="price", required=false) String price,
-                                          @RequestParam(name="execInst", required=false) String execInst,
-                                          @RequestParam(name="stopPx", required = false) String stopPx,
-                                          @RequestParam(name="leverage", required = false) String leverage,
-                                          Authentication authentication) {
-
-      User trader = ((CustomUserDetails) authentication.getPrincipal()).getUser();
-
-      DataPostLeverage dataLeverageBuilder = new DataPostLeverage()
-              .withSymbol(Symbol.valueOf(symbol).getValue())
-              .withLeverage(leverage);
-
-      DataPostOrderBuilder dataOrderBuilder = new DataPostOrderBuilder()
-              .withSymbol(Symbol.valueOf(symbol).getValue())
-              .withSide(side).withOrderType(ordType)
-              .withPrice(price).withExecInst(execInst).withStopPrice(stopPx);
-
-      tradeService.placeOrderAll(trader, dataLeverageBuilder, dataOrderBuilder);
-
-      return new ResponseEntity<>("okk", HttpStatus.OK);
-    }
-
-  @DeleteMapping("/position")
-  public ResponseEntity<?> postPosition(@RequestParam(name="symbol", required = false) String symbol,
-                                          Authentication authentication)
-  {
-    User trader = ((CustomUserDetails) authentication.getPrincipal()).getUser();
-
-    DataPostOrderBuilder dataPostOrderBuilder = new DataPostOrderBuilder()
-            .withSymbol(symbol)
-            .withOrderType("Market")
-            .withExecInst("Close");
-
-    tradeService.closeAllPosition(trader, dataPostOrderBuilder);
-
-    return new ResponseEntity<>("okk", HttpStatus.OK);
   }
 
 
