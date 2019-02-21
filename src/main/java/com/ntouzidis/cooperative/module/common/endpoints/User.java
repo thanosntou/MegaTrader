5a306b02b2243407fc8c707b2260def7d0359201
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-02-22 01:20:42 +0200

    bitmex api endpoints

--- src/main/java/com/ntouzidis/cooperative/module/common/endpoints/User.java
+++ src/main/java/com/ntouzidis/cooperative/module/common/endpoints/User.java
@@ -1,4 +1,23 @@
 package com.ntouzidis.cooperative.module.common.endpoints;
 
-public class User {
+public class User extends BitmexAPI {
+
+    public static final String USER_= "/api/v1/user";
+    public static final String USER_MARGIN_= "/api/v1/user/margin";
+    public static final String USER_WALLET_= "/api/v1/user/wallet";
+    public static final String USER_WALLET_HISTORY_= "/api/v1/user/walletHistory";
+    public static final String USER_WALLET_SUMMARY_= "/api/v1/user/walletSummary";
+    public static final String USER_COMMISSION_= "/api/v1/user/commission";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
+//    public static final String USER_= "/api/v1/user";
 }
