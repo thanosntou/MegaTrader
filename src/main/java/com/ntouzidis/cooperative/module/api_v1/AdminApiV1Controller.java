fa5fe1aee592c49056236fcafa56977f535b3aa9
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-02-24 00:09:33 +0200

    totalBalance

--- src/main/java/com/ntouzidis/cooperative/module/api_v1/AdminApiV1Controller.java
+++ src/main/java/com/ntouzidis/cooperative/module/api_v1/AdminApiV1Controller.java
@@ -3,9 +3,11 @@ package com.ntouzidis.cooperative.module.api_v1;
 import com.google.common.base.Preconditions;
 import com.ntouzidis.cooperative.module.user.entity.CustomUserDetails;
 import com.ntouzidis.cooperative.module.user.entity.Login;
+import com.ntouzidis.cooperative.module.user.entity.User;
 import com.ntouzidis.cooperative.module.user.repository.LoginRepository;
 import com.ntouzidis.cooperative.module.user.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,9 +31,12 @@ public class AdminApiV1Controller {
         this.userService = userService;
     }
 
-    @GetMapping("/logins")
-    public ResponseEntity<List<Login>> readLogins(Authentication authentication
-    ) {
+    @GetMapping(
+            value = "/logins",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<Login>> readLogins(Authentication authentication) {
+
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
 
         Preconditions.checkArgument(userService.isAdmin(userDetails.getUser()));
@@ -42,5 +47,20 @@ public class AdminApiV1Controller {
                 .collect(Collectors.toList());
 
         return new ResponseEntity<>(logins, HttpStatus.OK);
+    }
+
+    @GetMapping(
+            value = "/totalBalance",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Double> calculateTotalBalance(Authentication authentication) {
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        Preconditions.checkArgument(userService.isAdmin(userDetails.getUser()));
+
+        double totalBalance = userService.calculateTotalBalance();
+
+        return new ResponseEntity<>(totalBalance, HttpStatus.OK);
     }
 }
