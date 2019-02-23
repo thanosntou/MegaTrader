b3d386b2375c0d0dc53b09d2d826ebab612d4855
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2019-02-23 03:40:42 +0200

    multithreading

--- src/main/java/com/ntouzidis/cooperative/CooperativeApplication.java
+++ src/main/java/com/ntouzidis/cooperative/CooperativeApplication.java
@@ -3,12 +3,19 @@ package com.ntouzidis.cooperative;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.*;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CooperativeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CooperativeApplication.class, args);
+    }
+
+    @Bean(name = "multiExecutor")
+    public Executor multiExecutor() {
+        return Executors.newFixedThreadPool(50);
     }
 }
